81ff095edefe9f81c0cccdbe01549bbde8e31998
Salvador Guzman <guzmansalv@gmail.com>
2021-01-01 16:10:04 -0800

    fixed logging to use %s rather than f

--- src/initchans/main.py
+++ src/initchans/main.py
@@ -11,22 +11,25 @@ logging.info("Starting Channel List Initializer")
 csv_file_name = './channels.csv'
 
 csv_file = open(csv_file_name, 'r')
-logging.debug(f'Opening {csv_file_name}')
+logging.debug('Opening %s', csv_file_name)
 
 csv_reader = csv.reader(csv_file_name)
-logging.debug(f'Reading {csv_file_name}')
+logging.debug('Reading %s', csv_file_name)
 
 user = myutil.env.get_db_user()
 host = myutil.env.get_db_host()
 port = myutil.env.get_db_port()
 db = myutil.env.get_db_name()
+table = myutil.env.get_db_table()
 
+sql_insert = f'INSERT INTO {table} (serial) VALUES (%s);'
 conn = psycopg2.connect(user=user, host=host, port=port, database=db)
 cursor = conn.cursor()
 
 for row in csv_reader:
     serial = row[0]
-    print(serial)
+    logging.debug('Inserting value %s into table %s', serial, table)
+    cursor.exec(sql_insert, [serial])
 
 cursor.close()
 conn.close()
